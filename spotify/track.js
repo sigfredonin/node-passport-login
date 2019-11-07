0d5fc657f5ca8fdbeedd476f88217cf074b70bf3
Sig Nin <signin@email.com>
2019-11-07 14:58:39 -0800

    Add track duration, fix name labels on artist and track.

--- spotify/track.js
+++ spotify/track.js
@@ -1,12 +1,14 @@
+const TEST = process.env.TEST || false;
+
 const Album = require('./Album');
 
 const getDurationFromMilliseconds = (ms) => {
-  const msecs = ms % 1000;            // 7354320 % 1000 = 320 ms
-  const secs_whole = ms / 1000;       // 7354320 / 1000 = 7354 secs
-  const secs = secs_whole % 60;       // 7354 secs % 60 = 34 secs
-  const mins_whole = secs_whole / 60; // 7354 secs / 60 = 122 mins
-  const mins = mins_whole % 60;       // 122 mins % 60 = 2 mins
-  const hours = mins_whole / 60;      // 122 mins / 60 = 2 hrs
+  const msecs = ms % 1000;                        // 7354320 % 1000 = 320 ms
+  const secs_whole = Math.floor(ms / 1000);       // 7354320 / 1000 = 7354 secs
+  const secs = secs_whole % 60;                   // 7354 secs % 60 = 34 secs
+  const mins_whole = Math.floor(secs_whole / 60); // 7354 secs / 60 = 122 mins
+  const mins = mins_whole % 60;                   // 122 mins % 60 = 2 mins
+  const hours = Math.floor(mins_whole / 60);      // 122 mins / 60 = 2 hrs
   return ""
     + (hours < 10 ? "0" + hours : hours)
     + ":"
@@ -16,6 +18,9 @@ const getDurationFromMilliseconds = (ms) => {
     + "."
     + (msecs < 100 ? "0" + (msecs < 10 ? "0" + msecs : msecs) : (msecs < 10 ? "0" + msecs : msecs));
 }
+
+// Should print "TEST 7354320 msec -> 02:02:34.320" on the console log
+if (TEST) console.log("TEST 7354320 msec -> " + getDurationFromMilliseconds('7354320'));
 
 class Track {
   constructor (albumJSON) {
